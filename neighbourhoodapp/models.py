a84c2bab1b24e1a06f44a603d6000f27acfd1cab
Robert <rgitonga.07@gmail.com>
2022-06-19 01:43:29 +0300

    add neighbourhood class methods

--- neighbourhoodapp/models.py
+++ neighbourhoodapp/models.py
@@ -10,3 +10,15 @@ class Neighbourhood(models.Model):
     health_number = models.IntegerField(null=True, blank=True)
     police_number = models.IntegerField(null=True, blank=True)
     occupant_count = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}hood'
+
+    def save_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        self.delete()
+    @classmethod
+    def find_neighbourhood(cls, neighbourhood_id):
+        return cls.objects.filter(id=neighbourhood_id)
