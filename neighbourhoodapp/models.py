b9f83a4588a92cc3593b7ce937c1f8f6072d06c0
Robert <rgitonga.07@gmail.com>
2022-06-19 01:41:52 +0300

    add neighbourhood model

--- neighbourhoodapp/models.py
+++ neighbourhoodapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    admin = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='hood')
+    neighbourhood_logo = CloudinaryField('image')
+    description = models.TextField()
+    health_number = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+    occupant_count = models.IntegerField(null=True, blank=True)
