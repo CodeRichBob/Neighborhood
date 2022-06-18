cc9ca1e41b1a0d25006bb31348166bd7cf628e6f
Robert <rgitonga.07@gmail.com>
2022-06-19 02:53:41 +0300

    add url pattern for creating a neighbourhood

--- neighbourhoodapp/urls.py
+++ neighbourhoodapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='hood-home'),
     path('all-hoods/',views.neighbourhoods,name='hood'),
+    path('new-hood/', views.create_neighbourhood, name='new-hood'),
 ]
