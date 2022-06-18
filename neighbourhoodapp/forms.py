9607b5e0d32fa5a3f7923379fb3c998a9667d987
Robert <rgitonga.07@gmail.com>
2022-06-19 02:40:53 +0300

    add form classes

---
+++ neighbourhoodapp/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Neighbourhood, Profile, Post, Business
+
+
+class NeighbourHoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        exclude = ('admin',)
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user', 'neighbourhood')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
