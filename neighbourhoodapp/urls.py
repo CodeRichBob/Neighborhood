93e9c5e7440addeb9e950328600e2617af3abfdd
Robert <rgitonga.07@gmail.com>
2022-06-19 03:05:26 +0300

    add url for joining a neighbourhood

--- neighbourhoodapp/urls.py
+++ neighbourhoodapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='hood-home'),
     path('all-hoods/',views.neighbourhoods,name='hood'),
     path('new-hood/', views.create_neighbourhood, name='new-hood'),
+    path('join_hood/<id>', views.join_neighbourhood, name='join-hood'),
 ]
