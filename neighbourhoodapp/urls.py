d27c75ae0c4ac2322b163ee94117a9315d5bb407
Robert <rgitonga.07@gmail.com>
2022-06-19 03:27:12 +0300

    add template

--- neighbourhoodapp/urls.py
+++ neighbourhoodapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('new-hood/', views.create_neighbourhood, name='new-hood'),
     path('join_hood/<id>', views.join_neighbourhood, name='join-hood'),
     path('leave_hood/<id>', views.leave_neighbourhood, name='leave-hood'),
+    path('single_hood/<hood_id>', views.single_neighbourhood, name='single-hood'),
+    path('<hood_id>/new-post', views.create_post, name='post'),
 ]
--- neighbourhoodapp/views.py
+++ neighbourhoodapp/views.py
@@ -39,3 +39,44 @@ def leave_neighbourhood(request, id):
     request.user.profile.neighbourhood = None
     request.user.profile.save()
     return redirect('hood')
+
+def single_neighbourhood(request, hood_id):
+    hood = Neighbourhood.objects.get(id=hood_id)
+    business = Business.objects.filter(neighbourhood=hood)
+    posts = Post.objects.filter(hood=hood)
+    posts = posts[::-1]
+    if request.method == 'POST':
+        form = BusinessForm(request.POST)
+        if form.is_valid():
+            b_form = form.save(commit=False)
+            b_form.neighbourhood = hood
+            b_form.user = request.user.profile
+            b_form.save()
+            return redirect('single-hood', hood.id)
+    else:
+        form = BusinessForm()
+    params = {
+        'hood': hood,
+        'business': business,
+        'form': form,
+        'posts': posts
+    }
+    return render(request, 'neighbourhood/single_hood.html', params)
+
+
+def create_post(request, hood_id):
+    hood = Neighbourhood.objects.get(id=hood_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.hood = hood
+            post.user = request.user.profile
+            post.save()
+            return redirect('single-hood', hood.id)
+    else:
+        form = PostForm()
+    return render(request, 'neighbourhood/post.html', {'form': form})
+
+def map(request):
+    return render(request, 'neighbourhood/map.html')
