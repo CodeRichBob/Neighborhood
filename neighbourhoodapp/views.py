5c3bdf9a70cbe611c85791699b2fb8f33a34310e
Robert <rgitonga.07@gmail.com>
2022-06-19 03:06:47 +0300

    add view function for joining a neighbourhood

--- neighbourhoodapp/views.py
+++ neighbourhoodapp/views.py
@@ -27,3 +27,9 @@ def create_neighbourhood(request):
     else:
         form = NeighbourHoodForm()
     return render(request, 'neighbourhood/newhood.html', {'form': form})
+
+def join_neighbourhood(request, id):
+    neighbourhood = get_object_or_404(Neighbourhood, id=id)
+    request.user.profile.neighbourhood = neighbourhood
+    request.user.profile.save()
+    return redirect('hood')
